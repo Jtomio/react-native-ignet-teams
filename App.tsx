9c484dd8032e5e8b87df98126d94ffdc1e6558c3
Jeison Tomio <medriase@gmail.com>
2023-01-30 15:10:57 -0300

    css refatorado

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { Groups } from '@screens/Groups';
+import { ThemeProvider } from 'styled-components'
+import { StatusBar } from 'react-native';
+import theme from './src/theme'
+import { Loading } from '@components/Loading';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+// import { NewGroup } from '@components/NewGroup';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+
+  );
+}
+
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,43 @@
+
+import { useState } from 'react';
+import { GroupCard } from '@components/GoupCard';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { Container } from './styles'
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+
+
+export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
+  return (
+    <Container >
+      <Header />
+      <Highlight
+        title='Turmas'
+        subtitle='Jogue com a sua turma'
+      />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message="Que tal cadastrar nova turma?"
+          />)}
+      />
+
+      <Button
+        title="Criar nova turma"
+      />
+    </Container>
+  );
+}
